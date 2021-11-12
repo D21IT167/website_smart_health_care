a1ba43feca60a9d503c7e9927daf2bd3aa0f7d73
unknown <d21it167@charusat.edu.in>
2021-11-12 14:42:22 +0530

    Initial commit

---
+++ about.php
@@ -0,0 +1,50 @@
+<?php include('header.php'); ?>
+
+
+
+
+
+	<!-- this is for donor registraton -->
+	<div class="main_content" style="background-color:#fff;">
+		<h3 class="text-center" style="background-color:#272327;color: #fff;">About Us</h3>
+		<div class="col-md-12">
+			<div class="col-md-6">
+				<img src="img/aboutimag33.jpg" alt="" class="img-responsive">
+			</div>
+
+			<div class="col-md-6">
+				<article>
+					<h3 style="color:#0616BC;">Smart Health Care System</h3>
+					<p class="text-justify">Hello <br> This project is build up by Angi shah D21IT167 & Yash Agarwal D21IT178. <br>Thank you for visiting to this website.</p>
+				</article>
+			</div>
+		</div>
+
+
+		
+          
+    </div>
+		
+	
+	
+	
+
+	
+ 
+
+
+	
+	</div><!--  containerFluid Ends -->
+
+
+
+
+	<script src="js/bootstrap.min.js"></script>
+
+
+
+
+
+	
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,29 @@
+<?php include('header.php'); ?>
+
+	<!-- this is for welcome -->
+	<div class="content" style="font-size: medium;">
+		
+			
+			Welcome to the Smart Health Care System.<br>
+			Please choose your desired service.
+			
+		
+
+
+	
+	</div><br>
+
+	
+		
+
+          
+   
+
+
+	<script src="js/jquery-1.9.0.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+
+	 
+	
+</body>
+</html>
